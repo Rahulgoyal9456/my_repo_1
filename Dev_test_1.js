2025e3c0423229dc3671dc3347192316303ffd74
Rahulgoyal9456 <goyamraul56@gmail.com>
2025-07-08 15:04:22 +0530

    Update Dev_test_1.js

--- Dev_test_1.js
+++ Dev_test_1.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Hello YouTube Test
 // @match        *://www.youtube.com/*
-// @downloadURL  https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Shorts%20viewer.js
-// @updateURL    https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Shorts%20viewer.js
+// @downloadURL  https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Dev_test_1.js
+// @updateURL    https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Dev_test_1.js
 // @grant        none
 // ==/UserScript==
 
