98cc2eb142ed2328193c73bf3fddca821f14f31b
Rahulgoyal9456 <goyamraul56@gmail.com>
2025-07-08 15:16:50 +0530

    Update Dev_test_1.js

--- Dev_test_1.js
+++ Dev_test_1.js
@@ -3,6 +3,7 @@
 // @match        *://www.youtube.com/*
 // @match        *://www.yahoo.com/*
 // @match        *://m.youtube.com/*
+// @match        *://example.com/*
 // @downloadURL  https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Dev_test_1.js
 // @updateURL    https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Dev_test_1.js
 // @grant        none
