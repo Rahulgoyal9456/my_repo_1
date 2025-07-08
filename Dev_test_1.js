3b004388db18d8cb2f2d5bc45b33267e4993eda9
Rahulgoyal9456 <goyamraul56@gmail.com>
2025-07-08 15:03:43 +0530

    Create Dev_test_1.js

---
+++ Dev_test_1.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Hello YouTube Test
+// @match        *://www.youtube.com/*
+// @downloadURL  https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Shorts%20viewer.js
+// @updateURL    https://raw.githubusercontent.com/Rahulgoyal9456/my_repo_1/main/Shorts%20viewer.js
+// @grant        none
+// ==/UserScript==
+
+alert("Tampermonkey is working!");
